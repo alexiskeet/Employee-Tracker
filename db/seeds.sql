986eb54340b863b033acc5b4d7adf8848cedfe6b
Alexis <alexis_keeter@yahoo.com>
2023-05-05 13:39:46 -0700

    added sample seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_name)
+VALUES ("Customer Service");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Cashier", 30000, 1);
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ("Jane", "Doe", 1, 1);
